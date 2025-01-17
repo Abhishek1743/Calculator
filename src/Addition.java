6f347bffdb336f5e8117bb2aa3ef254543c001a9
Abhishek1743 <abhisheksagar1113@gmail.com>
2025-01-17 14:53:53 +0530

    About to add

---
+++ src/Addition.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number for addition");
+        int a = sc.nextInt();
+        System.out.println("Enter second number for addition");
+        int b = sc.nextInt();
+        System.out.println("Enter third number for addition");
+        int c = sc.nextInt();
+        System.out.println("The sum is " + (a + b + c));
+    }
+}
